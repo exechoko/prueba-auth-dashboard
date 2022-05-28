d16a52e17af34ab17b19ab5748c130b1dd4d7acf
exe <exemarichal@gmail.com>
2022-05-28 14:35:56 -0300

    Se agrego el login de material template

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravel\Fortify\Fortify;
+
 return [
 
     /*
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
 
     ],
 
